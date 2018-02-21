16ad4df9081a4bb930752d4e18b9d8f246552485
NahuelMuller <nahuelam.93@gmail.com>
2018-02-21 13:15:53 -0300

    added interrupt handlers

--- src/main.c
+++ src/main.c
@@ -7,12 +7,45 @@
 /*------ LIBRARIES ------*/
 #include  "main.h"
 
-int main(void) {
+/*------ MAIN ------*/
+int main(void){
 
   leds_init();
   tecs_init();
 
-  while (1) {
+  while (1){
   }
   return 0;
 }
+
+/*------ INTERRUPTS ------*/
+void DMA_IRQHandler(){    // Interrupcion del DMA
+}
+void GPIO0_IRQHandler(){  // Interrupcion asignada a TEC_1
+  LPC_GPIO_PIN_INT->IST |= (0x01 << 0); // Clear Interrupt
+}
+void GPIO1_IRQHandler(){  // Interrupcion asignada a TEC_2
+  LPC_GPIO_PIN_INT->IST |= (0x01 << 1);	// Clear Interrupt
+}
+void GPIO2_IRQHandler(){  // Interrupcion asignada a TEC_3
+  LPC_GPIO_PIN_INT->IST |= (0x01 << 2);	// Clear Interrupt
+}
+void GPIO3_IRQHandler(){  // Interrupcion asignada a TEC_4
+  LPC_GPIO_PIN_INT->IST |= (0x01 << 3);	// Clear Interrupt
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*------ END ------*/
